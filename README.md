e8fd546183e6fdc90960c5b366bed64ef08e6874
Cemali Gencer <cemali.gencer@gmail.com>
2022-12-04 05:23:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# TK-Design System
+
+Vue 3 versiyonu üzerinde, Design System denemesi yapildi. 
+BEM (Block Element Modifier) css metodolojisi, Card bileşeninde kullanıldı.
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/components/Card.vue
@@ -0,0 +1,94 @@
+<script >
+export default {
+  name: "Card"
+};
+</script>
+
+<template>
+  <div>
+    <div class="some-page-wrapper">
+      <div class="row">
+        <div class="column">
+          <section class="card-container">
+            <article class="card">
+              <div class="c-card__body">
+                <div class="c-card__media">
+                  <div class="c-card__media-left">
+                    <figure class="c-card__media-image">
+                      <img
+                        src="@/assets/img/ek-hizmetler.svg"
+                        alt="Placeholder image"
+                      />
+                    </figure>
+                  </div>
+                  <div class="c-card__media-right">
+                    <p class="c-card__title">Ek Hizmetler</p>
+                    <p class="c-card__content">
+                      Seyahatinizi zenginleştirmenizi sağlayacak otel
+                      rezervasyonu ve araç kiralama gibi hizmetlerimizden
+                      faydalanabilirsiniz.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+            </article>
+          </section>
+        </div>
+        <div class="column">
+          <section class="card-container">
+            <article class="card">
+              <div class="c-card__body">
+                <div class="c-card__media">
+                  <div class="c-card__media-left">
+                    <figure class="c-card__media-image">
+                      <img
+                        src="@/assets/img/seyahat-kisitlamalari.svg"
+                        alt="Placeholder image"
+                      />
+                    </figure>
+                  </div>
+                  <div class="c-card__media-right">
+                    <p class="c-card__title">Seyahat gereksinimleri</p>
+                    <p class="c-card__content">
+                      İnteraktif harita uygulamamızı kullanarak ülkelerin güncel seyahat gereksinimlerine erişebilirsiniz.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+            </article>
+          </section>
+        </div>
+        <div class="column">
+          <section class="card-container">
+            <article class="card">
+              <div class="c-card__body">
+                <div class="c-card__media">
+                  <div class="c-card__media-left">
+                    <figure class="c-card__media-image">
+                      <img
+                        src="@/assets/img/kampanya.svg"
+                        alt="Placeholder image"
+                      />
+                    </figure>
+                  </div>
+                  <div class="c-card__media-right">
+                    <p class="c-card__title">Turkish Airlines Gift Card</p>
+                    <p class="c-card__content">
+                      Turkish Airlines Gift Card satın alarak sevdiklerinize sürpriz bir seyahat deneyimi armağan edebilir, onlara yeni yerler keşfetmenin heyecanını yaşatabilirsiniz.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+            </article>
+          </section>
+        </div>                
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
